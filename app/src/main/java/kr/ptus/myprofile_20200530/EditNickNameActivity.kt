19b6901b116e16db97ed88641e1497a968048d2b
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-30 12:22:32 +0900

    닉네임 변경과 전화번호 입력 후 전화걸기 직전까지

--- app/src/main/java/kr/ptus/myprofile_20200530/EditNickNameActivity.kt
+++ app/src/main/java/kr/ptus/myprofile_20200530/EditNickNameActivity.kt
@@ -1,12 +1,31 @@
 package kr.ptus.myprofile_20200530
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+
+        okBtn.setOnClickListener {
+
+            val myIntent = inputContent.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", myIntent)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()
+
+        }
+
+
     }
 }
--- app/src/main/java/kr/ptus/myprofile_20200530/MainActivity.kt
+++ app/src/main/java/kr/ptus/myprofile_20200530/MainActivity.kt
@@ -1,6 +1,8 @@
 package kr.ptus.myprofile_20200530
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,11 +18,35 @@ class MainActivity : AppCompatActivity() {
         editNickNameBtn.setOnClickListener {
 
             val myIntent = Intent(this, EditNickNameActivity::class.java)
-            startActivityForResult(myIntent,REQ_FOR_NICKNAME)
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
+
+        }
+
+        val myUri = Uri.parse("tel:${inputPhoneNumTxt}")
+        val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+        startActivity(myIntent)
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_FOR_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                val newNickname = data?.getStringExtra("nick")
+
+                myNickNameTxt.text = newNickname
+
+
+            }
+
 
         }
 
     }
-
 
 }
